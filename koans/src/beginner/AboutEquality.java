aa2d15e30a76bf2fc138979c644de00c0bdcdc16
rubytester <github@rubytester.com>
2013-08-29 18:11:39 -0500

    Equality done. one strange example here

--- koans/src/beginner/AboutEquality.java
+++ koans/src/beginner/AboutEquality.java
@@ -11,33 +11,38 @@ public class AboutEquality {
 	public void doubleEqualsTestsIfTwoObjectsAreTheSame(){
 		Object object = new Object();
 		Object sameObject = object;
-		assertEquals(object == sameObject, __);
-		assertEquals(object == new Object(), __);
+        boolean compare = object == sameObject;
+		assertEquals(compare,true);
+        boolean compare_again = object == new Object();
+		assertEquals(compare_again, false);
 	}
 
 	@Koan
 	public void equalsMethodByDefaultTestsIfTwoObjectsAreTheSame(){
 		Object object = new Object();
-		assertEquals(object.equals(object), __);
-		assertEquals(object.equals(new Object()), __);
-	}
+		assertEquals(object.equals(object), true);
+		assertEquals(object.equals(new Object()), false);
+    }
 
 	@Koan
 	public void equalsMethodCanBeChangedBySubclassesToTestsIfTwoObjectsAreEqual(){
 		Object object = new Integer(1);
-		assertEquals(object.equals(object), __);
-		assertEquals(object.equals(new Integer(1)), __);
+		assertEquals(object.equals(object), true);
+        Object o = new Integer(1); //let's play
+        assertEquals(object.equals(o), true); //ok so it's true but was false in previous example above
+
+		assertEquals(object.equals(new Integer(1)), true); // I thought it should be false
 		// Note: This means that for the class 'Object' there is no difference between 'equal' and 'same'
 	}
 
 	@Koan
 	public void objectsNeverEqualNull(){
-		assertEquals(new Object().equals(null), __);
+		assertEquals(new Object().equals(null), false);
 	}
 
 	@Koan
 	public void objectsEqualThemselves(){
 		Object obj = new Object();
-		assertEquals(obj.equals(obj), __);
+		assertEquals(obj.equals(obj), true); // duh. we did this example above
 	}
 }

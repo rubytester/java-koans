7a349e7066827ea484ccf663c647f313999b7654
rubytester <git@rubytester.com>
2013-09-01 17:46:23 -0500

    Objects done. is that all there was?

--- koans/src/beginner/AboutObjects.java
+++ koans/src/beginner/AboutObjects.java
@@ -13,16 +13,19 @@ public class AboutObjects {
 
 	@Koan
 	public void newObjectInstancesCanBeCreatedDirectly() {
-		assertEquals(new Object() instanceof Object, __);
+		assertEquals(new Object() instanceof Object, true);
+        Object o = new Object();
+        assertEquals(o instanceof Object, true); //instanceof is a keyword then aha! but why in this example
+        // Why am I learnign instanceof here? why do I even need it? arghhh!
 	}
 
 	@Koan
 	public void allClassesInheritFromObject() {
 		class Foo {}
 
-		Class<?>[] ancestors = getAncestors(new Foo());
-		assertEquals(ancestors[0], __);
-		assertEquals(ancestors[1], __);
+		Class<?>[] ancestors = getAncestors(new Foo());  //what the hell is this notation? array of classes?
+		assertEquals(ancestors[0], Foo.class);
+		assertEquals(ancestors[1], Object.class);
 	}
 
 	@Koan
@@ -30,28 +33,33 @@ public class AboutObjects {
 		Object object = new Object();
 		// TODO: Why is it best practice to ALWAYS override toString?
 		String expectedToString = MessageFormat.format("{0}@{1}", Object.class.getName(), Integer.toHexString(object.hashCode()));
-		assertEquals(expectedToString, __); // hint: object.toString()
+		assertEquals(expectedToString, object.toString()); // hint: object.toString()
+        //ClassName@hash. visit the Object.toString comments in source code
 	}
 
 	@Koan
 	public void toStringConcatenates(){
 		final String string = "ha";
+
 		Object object = new Object(){
 			@Override public String toString(){
 				return string;
 			}
 		};
-		assertEquals(string + object, __);
+		assertEquals(string + object, "haha"); // I don't get it.
+        // we override toString method of an Object which should return the String object and teh actual string is 'ha'
 	}
 
 	@Koan
 	public void toStringIsTestedForNullWhenInvokedImplicitly(){
 		String string = "string";
-		assertEquals(string+null, __);
-	}
+		assertEquals(string+null, "stringnull"); // is it because toString for null is "null"
+
+        // why is it "stringnull"?
+    }
 
 	private Class<?>[] getAncestors(Object object) {
-		List<Class<?>> ancestors = new ArrayList<Class<?>>();
+		List<Class<?>> ancestors = new ArrayList<Class<?>>(); // I am noob at this.
 		Class<?> clazz = object.getClass();
 		while(clazz != null) {
 			ancestors.add(clazz);

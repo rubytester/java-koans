e07970f3fc3e94335780a4427275555bab2c68ef
rubytester <git@rubytester.com>
2013-09-01 22:40:42 -0500

    Casting. deciphering all along

--- koans/src/beginner/AboutCasting.java
+++ koans/src/beginner/AboutCasting.java
@@ -1,8 +1,7 @@
 package beginner;
 
 import static com.sandwich.koan.constant.KoanConstants.__;
-import static com.sandwich.util.Assert.assertEquals;
-import static com.sandwich.util.Assert.fail;
+import static com.sandwich.util.Assert.*;
 
 import com.sandwich.koan.Koan;
 
@@ -14,17 +13,23 @@ public class AboutCasting {
 		int a = 6;
 		long b = 10;
 		Object c = a + b;
-		assertEquals(c, __);
-		assertEquals(c instanceof Integer, __);
-		assertEquals(c instanceof Long, __);
-	}
+		assertEquals(c, 16L);
+		assertEquals(c instanceof Integer, false); // setting false here is not nice. code smell
+        assertEquals(c instanceof Long, true);
+        assertEquals(c.getClass().getName(), "java.lang.Long" ); // this tell me the full name of class
+        assertEquals(c.getClass(), Long.class);  // this compares to Long.class
+
+    }
 
 	@Koan
 	public void forceIntTypecast() {
 		long a = 2147483648L;
 		// What happens if we force a long value into an int?
 		int b = (int) a;
-		assertEquals(b, __);
+		assertEquals(b, Integer.MIN_VALUE); // we overflow it?
+
+        assertTrue(a > Integer.MAX_VALUE); // well, if Float is greater than Integer we should not cast it
+        assertFalse(a < Integer.MIN_VALUE && a > Integer.MAX_VALUE); // not safe to (int) a
 	}
 
 	@Koan
@@ -32,7 +37,16 @@ public class AboutCasting {
 		int a = 1;
 		int b = Integer.MAX_VALUE;
 		long c = a + b; // still overflows int... which is the Integer.MIN_VALUE, the operation occurs prior to assignment to long
-		assertEquals(c, __);
+		assertEquals(c, (long) Integer.MIN_VALUE);
+
+        // we could show overflow on int first
+        int ab = a + b;
+        assertEquals(ab, Integer.MIN_VALUE);
+
+        // and then
+        long abl = (long) a + (long) b;
+        assertTrue(abl > Integer.MAX_VALUE); // compare what is greater
+
 	}
 
 	interface Sleepable {
@@ -61,10 +75,16 @@ public class AboutCasting {
 	public void downCastWithInheritance() {
 		Child child = new Child();
 		Parent parentReference = child; // Why isn't there an explicit cast?
-		assertEquals(child instanceof Child, __);
-		assertEquals(parentReference instanceof Child, __);
-		assertEquals(parentReference instanceof Parent, __);
-		assertEquals(parentReference instanceof Grandparent, __);
+                                        // cause it a super type yes?
+		assertEquals(child instanceof Child, true);
+        assertEquals(child.getClass(), Child.class);
+
+        assertEquals(parentReference instanceof Child, true);
+        assertEquals(parentReference.getClass(), Child.class);
+
+
+        assertEquals(parentReference instanceof Parent, true); // cause it's an ancestor
+		assertEquals(parentReference instanceof Grandparent, true); // kind_of? in ruby
 	}
 
 	@Koan
@@ -72,17 +92,18 @@ public class AboutCasting {
 		Child child = new Child();
 		Parent parentReference = child;
 		// If the result is unexpected, consider the difference between an instance and its reference
-		assertEquals(parentReference.complain(), __);
+		assertEquals(parentReference.complain(), child.complain());
 	}
 
 	@Koan
 	public void upCastWithInheritance() {
 		Grandparent child = new Child();
 		Parent parentReference = (Parent) child; // Why do we need an explicit cast here?
+                                                 //  I don't know. Why do we need it?
 		Child childReference = (Child) parentReference; // Or here?
-		assertEquals(childReference instanceof Child, __);
-		assertEquals(childReference instanceof Parent, __);
-		assertEquals(childReference instanceof Grandparent, __);
+		assertEquals(childReference instanceof Child, true);
+		assertEquals(childReference instanceof Parent, true);
+		assertEquals(childReference instanceof Grandparent, true);
 	}
 
 	@Koan
@@ -91,7 +112,7 @@ public class AboutCasting {
 		Parent parent = (Child) child;
 		// Think about the result. Did you expect that? Why?
 		// How is that different from above?
-		assertEquals(parent.complain(), __);
+		assertEquals(parent.complain(), ((Child) child).complain()); // o my this is so confusing
 	}
 
 	@Koan
@@ -100,7 +121,8 @@ public class AboutCasting {
 			Object o = new Object();
 			((Sleepable) o).sleep(); // would this even compile without the cast?
 		} catch (ClassCastException x) {
-			fail("Object does not implement Sleepable, maybe one of the people classes do?");
+            // we do in fact get ClassCastException
+			//fail("Object does not implement Sleepable, maybe one of the people classes do?");
 		}
 	}
 
@@ -108,7 +130,7 @@ public class AboutCasting {
 	public void complicatedCast() {
 		Grandparent parent = new Parent();
 		// How can we access the parent's ability to "complain" - if the reference is held as a superclass?
-		assertEquals("TPS reports don't even have a cover letter!", __);
+		assertEquals("TPS reports don't even have a cover letter!", ((Parent) parent).complain()); // this is just silly I think
 	}
 
 }
